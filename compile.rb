dbc7dcb016ad70fa5aff928eea4508c0e65b56db
John Wilkinson <jcwilk@gmail.com>
2016-12-26 23:04:55 +0800

    Add makeshift compile stuff

---
+++ compile.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+require 'pry'
+
+
+def extract_from_file(filename)
+  reading = false
+  lines = []
+  File.readlines(filename).each do |line|
+    if !reading
+      if line =~ /-- ?START LIB/
+        reading = true
+      end
+      next
+    end
+    return lines if line =~ /^-- ?END LIB/
+    lines << line
+  end
+  lines
+end
+
+tmp = File.open('out.tmp','w')
+inserting = false
+File.readlines('material_girl.p8').each do |line|
+  if !inserting
+    tmp << line
+    if line =~ /-- ?START EXT ([^ ]+)\w*$/
+      extract_from_file($1.strip).each {|l| tmp << l }
+      inserting = true
+    end
+  elsif line =~ /-- ?END EXT/
+    tmp << line
+    inserting = false
+  end
+end
+tmp.close
+FileUtils.mv('out.tmp','material_girl.p8')
+
+
+
+
+
+
